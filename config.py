b2b565ed9108579d2197c13c3207088bab459e5e
ycmejia <yarleyc.mejiab@uqvirtual.edu.co>
2025-09-21 09:05:26 -0500

    Configuraciones de entorno UBUNTU y ejecucion correcta del proyecto

--- config.py
+++ config.py
@@ -1,16 +1,24 @@
 # config.py
-# === RUTAS Y CONFIG ===
+# === CONFIGURACIÓN DEL PROYECTO ===
 
-# Chromedriver
-CHROMEDRIVER_PATH = r"C:\Users\USER\Desktop\YAN\Carpeta Universidad\decimo-semestre\Analisis-de-algoritmos\Proyecto-final-algoritmos\chromedriver.exe"
+import platform
+import os
 
-# Descargas por fuente
-DOWNLOAD_DIR_SAGE = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\Sage_Journals"
-DOWNLOAD_DIR_SCIENCEDIRECT = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\science_direct"
+# === RUTAS SEGÚN EL SISTEMA OPERATIVO ===
+if platform.system() == "Windows":
+    # Windows
+    CHROMEDRIVER_PATH = r"C:\Users\USER\Desktop\YAN\Carpeta Universidad\decimo-semestre\Analisis-de-algoritmos\Proyecto-final-algoritmos\chromedriver.exe"
+    DOWNLOAD_DIR_SAGE = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\Sage_Journals"
+    DOWNLOAD_DIR_SCIENCEDIRECT = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\science_direct"
+else:
+    # Linux / macOS
+    CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+    DOWNLOAD_DIR_SAGE = os.path.expanduser("~/Descargas/Sage_Journals")
+    DOWNLOAD_DIR_SCIENCEDIRECT = os.path.expanduser("~/Descargas/science_direct")
 
-# CRAI (no lo usamos directo en este paso)
+# === URLS IMPORTANTES ===
 URL_LOGIN = "https://library.uniquindio.edu.co/databases"
 
-# Credenciales UQ
-USUARIO = "yfgomezc@uqvirtual.edu.co"
-CONTRASENA = "pass"
+# === CREDENCIALES ===
+USUARIO = "yarleyc.mejiab@uqvirtual.edu.co"
+CONTRASENA = "Familia967vfg15a"
