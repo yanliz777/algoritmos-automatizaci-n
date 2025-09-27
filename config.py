277906db27757336c986f6e6be03715e58526006
yan <yan_cristiano@hotmail.com>
2025-09-27 15:35:44 -0500

    Se realiza la automatización de la revista scienceDirect, 100 por página, se ralizan 5 páginas

--- config.py
+++ config.py
@@ -5,12 +5,15 @@
 CHROMEDRIVER_PATH = r"C:\Users\USER\Desktop\YAN\Carpeta Universidad\decimo-semestre\Analisis-de-algoritmos\Proyecto-final-algoritmos\chromedriver.exe"
 
 # Descargas por fuente
-DOWNLOAD_DIR_SAGE = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\Sage_Journals"
-DOWNLOAD_DIR_SCIENCEDIRECT = r"C:\Users\USER\Desktop\proyecto-final-algoritmos\science_direct"
+DOWNLOAD_DIR_SAGE = r"C:\Users\USER\Desktop\YAN\Carpeta Universidad\decimo-semestre\Analisis-de-algoritmos\Proyecto-final-algoritmos\bases_de_datos\Sage_Journals"
+DOWNLOAD_DIR_SCIENCEDIRECT = r"C:\Users\USER\Desktop\YAN\Carpeta Universidad\decimo-semestre\Analisis-de-algoritmos\Proyecto-final-algoritmos\bases_de_datos\science_direct"
 
 # CRAI (no lo usamos directo en este paso)
 URL_LOGIN = "https://library.uniquindio.edu.co/databases"
 
 # Credenciales UQ
 USUARIO = "yfgomezc@uqvirtual.edu.co"
-CONTRASENA = "pass"
+CONTRASENA = "jUpiter@52412"
+
+# URL ScienceDirect vía CRAI
+SCIENCEDIRECT_URL = "https://www-sciencedirect-com.crai.referencistas.com/"
